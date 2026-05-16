4416d823c2ea0f757e92ca44f6421525573f58c7
John Cowie Del Corral <john@go-atomic.io>
2026-05-16 14:23:07 +0200

    Fix typo

--- client/src/pages/GamePage.tsx
+++ client/src/pages/GamePage.tsx
@@ -8,7 +8,7 @@ import { useGameState } from '../hooks/useGameState'
 
 function roundRuleText(round: 1 | 2 | 3): string {
   if (round === 1) return 'Describe the concept by saying anything except the word itself! No Rhymes, "starts with _", or other obvious cheats.'
-  if (round === 2) return 'Charades! Act out the cooncept to get people to guess the word or phrase without saying anything or making any noises.'
+  if (round === 2) return 'Charades! Act out the concept to get people to guess the word or phrase without saying anything or making any noises.'
   return 'One word only - use one word to get the players to guess the correct answer'
 }
 
